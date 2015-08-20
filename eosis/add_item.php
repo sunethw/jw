8e6a98692587bf949a20a0f757d1a39eb33281b5
supungit <sthiyamba@gmail.com>
2015-08-20 16:16:38 +0530

    add customer and add item

--- eosis/add_item.php
+++ eosis/add_item.php
@@ -24,65 +24,58 @@
                      <div class="content-panel">
                       <h4><i class="fa fa-angle-right"></i>Item list </h4>
                          
-                         
+                         <div class="col-lg-12">
+                          <button href="#creatG4" class="panel-top-adbtn btn btn-success pull-left "><span class="btn-pls fa fa-plus"></span>Add New</button>
+                          </div>
                          
                           <section id="unseen">
                             <table class="table table-bordered table-striped table-condensed">
                               <thead>
                               <tr>
-                                  <th>Employee Name</th>
-                                 <th>Department</th>
-                                  <th class="numeric">NIC</th>
-                                  <th class="numeric">Join Date</th>
-                                  <th class="numeric">Employee ID</th>
-                                  <th class="numeric">Entitled</th>
-                                  <th class="numeric">Designation</th>
+                                  <th class="text-center">Item ID</th>
+                                  <th class="text-center">Item Name</th>
+                                  <th class="text-center">Item Quantity</th>
+                                  <th class="text-center">Gross weight(g)</th>
+                                  <th class="text-center">Reduction weight(g)</th>
+                                  <th class="text-center">Wastage</th>
+                                  <th class="text-center">Goldsmith Cost</th>
+                                 
                                   <th class="text-center">Action</th>
                               </tr>
                               </thead>
                               <tbody>
                               <tr>
-                                  <td>Suneth Wijesiri</td>
-                                  <td>2015/01/01</td>
-                                  <td class="numeric">Casual Leave</td>
-                                  <td class="numeric">Full Day</td>
-                                  <td class="numeric">Aminda</td>
-                                  <td class="numeric">21</td>
-                                  <td class="numeric">10</td>
+                                  <td class="text-center">001</td>
+                                  <td class="text-center">Neckless</td>
+                                  <td class="text-center">50</td>
+                                  <td class="text-center">100</td>
+                                  <td class="text-center">100</td>
+                                   <td class="text-center">100</td>
+                                  <td class="text-center">10000.00</td>
+                                  
                                   <td class="text-center"> 
-                                     <button class="btn btn-success btn-xs"><i class="fa fa-check"></i>&nbsp; Aprove</button>
-                                      <button class="btn btn-danger btn-xs"><i class="fa fa-close"></i>&nbsp; Reject</button>
-                                      <button class="btn btn-primary btn-xs"><i class="fa fa-eye"></i>&nbsp; View</button>
+                                     <button class="btn btn-primary btn-xs"><i class="fa fa-eye"></i>&nbsp; View</button>
+                                    <button class="btn btn-success btn-xs"><i class="fa fa-pencil"></i>&nbsp; Edit</button>
+                                      <button class="btn btn-danger btn-xs"><i class="fa fa-close"></i>&nbsp; Delete</button>
                                   </td>
                               </tr>
                               <tr>
-                                  <td>Suneth Wijesiri</td>
-                                  <td>2015/01/01</td>
-                                  <td class="numeric">Casual Leave</td>
-                                  <td class="numeric">Full Day</td>
-                                  <td class="numeric">Aminda</td>
-                                  <td class="numeric">21</td>
-                                  <td class="numeric">10</td>
+                                  <td class="text-center">001</td>
+                                  <td class="text-center">Neckless</td>
+                                  <td class="text-center">50</td>
+                                  <td class="text-center">100</td>
+                                  <td class="text-center">100</td>
+                                   <td class="text-center">100</td>
+                                  <td class="text-center">10000.00</td>
+                                  
                                   <td class="text-center"> 
-                                     <button class="btn btn-success btn-xs"><i class="fa fa-check"></i>&nbsp; Aprove</button>
-                                      <button class="btn btn-danger btn-xs"><i class="fa fa-close"></i>&nbsp; Reject</button>
-                                      <button class="btn btn-primary btn-xs"><i class="fa fa-eye"></i>&nbsp; View</button>
+                                     <button class="btn btn-primary btn-xs"><i class="fa fa-eye"></i>&nbsp; View</button>
+                                    <button class="btn btn-success btn-xs"><i class="fa fa-pencil"></i>&nbsp; Edit</button>
+                                      <button class="btn btn-danger btn-xs"><i class="fa fa-close"></i>&nbsp; Delete</button>
                                   </td>
                               </tr>
-                              <tr>
-                                  <td>Suneth Wijesiri</td>
-                                  <td>2015/01/01</td>
-                                  <td class="numeric">Casual Leave</td>
-                                  <td class="numeric">Full Day</td>
-                                  <td class="numeric">Aminda</td>
-                                  <td class="numeric">21</td>
-                                  <td class="numeric">10</td>
-                                  <td class="text-center"> 
-                                     <button class="btn btn-success btn-xs"><i class="fa fa-check"></i>&nbsp; Aprove</button>
-                                      <button class="btn btn-danger btn-xs"><i class="fa fa-close"></i>&nbsp; Reject</button>
-                                      <button class="btn btn-primary btn-xs"><i class="fa fa-eye"></i>&nbsp; View</button>
-                                  </td>
-                              </tr>
+                              
+                              
                             </tbody>  
                             </table> 
                         </section>     
@@ -91,64 +84,64 @@
             </div>     
                 <div class="row mt">
                     <div class="col-lg-12">
-                        <div class="form-panel">
+                        <div class="form-panel" id="creatG4">
                             <h4 class="mb"><i class="fa fa-angle-right"></i> Item Details</h4>
                             <form class="form-horizontal style-form">
                               
                                <div class="form-group">
-                                    <label class="col-sm-2 control-label">Item id</label>
+                                    <label class="col-sm-2 control-label required-label">Item id</label>
                                     <div class="col-sm-6">
-                                        <input type="text" class="form-control">
+                                        <input type="text" class="form-control required-input">
                                     </div>
                                     
                                    
                                 </div>
                                 
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Item Name</label>
+                                    <label class="col-sm-2 control-label required-label">Item Name</label>
                                     <div class="col-sm-6">
-                                        <input type="text" class="form-control">
+                                        <input type="text" class="form-control required-input">
                                     </div>
                                 </div>
                                 
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Item Quantity</label>
+                                    <label class="col-sm-2 control-label required-label">Item Quantity</label>
                                     <div class="col-sm-6">
-                                        <input type="text" class="form-control">
+                                        <input type="text" class="form-control required-input">
                                     </div>
                                 </div>
                                 
                                  <div class="form-group">
-                                    <label class="col-sm-2 control-label">Gross weight</label>
+                                    <label class="col-sm-2 control-label required-label required-label">Gross weight</label>
                                     <div class="col-sm-6">
-                                        <input type="text" class="form-control">
+                                        <input type="text" class="form-control required-input">
                                     </div>
                                 </div>
                                 
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Reduction weight</label>
+                                    <label class="col-sm-2 control-label required-label">Reduction weight</label>
                                     <div class="col-sm-6">
-                                        <input type="text" class="form-control">
+                                        <input type="text" class="form-control required-input">
                                     </div>
                                 </div>
                                 
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Wastage</label>
+                                    <label class="col-sm-2 control-label required-label">Wastage</label>
                                     <div class="col-sm-6">
-                                        <input type="text" class="form-control">
+                                        <input type="text" class="form-control required-input">
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Goldsmith Cost</label>
+                                    <label class="col-sm-2 control-label required-label">Goldsmith Cost</label>
                                     <div class="col-sm-6">
-                                        <input type="text" class="form-control">
+                                        <input type="text" class="form-control required-input">
                                     </div>
                                 </div>
                                 
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Desctiption</label>
+                                    <label class="col-sm-2 control-label required-label">Desctiption</label>
                                     <div class="col-sm-6">
-                                        <textarea class="form-control"></textarea>
+                                        <textarea class="form-control required-input"></textarea>
                                     </div>
                                 </div>
                                
@@ -193,7 +186,18 @@
     <!--common script for all pages-->
     <script src="assets/js/common-scripts.js"></script>
 
+    <script>
+    
+         $(document).on('click', '.panel-top-adbtn', function(){
 
+           $('#creatG4').collapse('show');
+           $("body, html").animate({ 
+                    scrollTop: $( $(this).attr('href') ).offset().top -140
+                }, 500);     
+ 
+        });
+    
+    </script>
 
        
        
