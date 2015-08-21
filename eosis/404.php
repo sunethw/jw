b32cd29c19f6e0b37fe67a0b7abda4e0b9703021
supungit <sthiyamba@gmail.com>
2015-08-21 10:13:23 +0530

    add comodity rates

---
+++ eosis/404.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   
+   <?php include 'header.php';?>
+    
+</head>
+
+<body>
+
+ 
+    
+    <?php // include 'component-tobar.php';?>
+    <?php //include 'component-sidebar.php';?> 
+
+        <section class="error-wrapper text-center ">
+            <h1><img alt="" src="assets/img/404-error.png"></h1>
+            <h2>page not found</h2>
+            <h3>We Couldn’t Find This Page</h3>
+            <a class="back-btn" href="index-2.html"> Back To Home</a>
+        </section>
+
+ 
+    <!--footer start-->
+    <?php include 'component-footer.php';?>  
+          <!--footer end-->
+    </section>
+
+    <!-- js placed at the end of the document so the pages load faster -->
+     <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery-ui-1.9.2.custom.min.js"></script>
+    <script src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="assets/js/jquery.scrollTo.min.js"></script>
+
+
+    <script src="assets/js/bootstrap-datetimepicker.js"></script>
+
+
+
+    <!--common script for all pages-->
+    <script src="assets/js/common-scripts.js"></script>
+
+      
+
+    
+   
+
+
+</body>
+
+</html>
--- eosis/header.php
+++ eosis/header.php
@@ -1,7 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
@@ -18,8 +14,3 @@
     <!-- Custom styles for this template -->
     <link href="assets/css/style.css" rel="stylesheet">
     <link href="assets/css/style-responsive.css" rel="stylesheet">
-</head>
-
-<body>
-
-<section id="container">
